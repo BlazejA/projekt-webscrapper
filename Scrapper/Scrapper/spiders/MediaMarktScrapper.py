252484c7fae18f2529e1ea118ae618a6e65a4e8f
DawidR2 <61984245+DawidR2@users.noreply.github.com>
2022-05-23 21:11:18 +0200

    MediaMarkt next page

--- Scrapper/Scrapper/spiders/MediaMarktScrapper.py
+++ Scrapper/Scrapper/spiders/MediaMarktScrapper.py
@@ -14,17 +14,20 @@ class MediaScrapper(scrapy.Spider):
 
     def parse(self, response):
 
-
         for products in response.css('div.offer'):
-
             yield {
                 'name': products.css('h2.title::text').get(),
-                'price': products.css('span.whole::text').get().strip('\n    ').strip('\n  '),
+                'price': products.css('span.whole::text').get().strip(),
                 'category': self.GetCategoryByProductName(products.css('h2.title::text').get()),
                 'link': products.css('div.info a').attrib['href']
             }
 
-    def GetCategoryByProductName(self,value: str) -> str:
+
+        next_page = response.css('div.more-offers a').attrib['href']
+        if next_page is not None:
+            yield response.follow("https://mediamarkt.pl"+next_page, callback=self.parse)
+
+    def GetCategoryByProductName(self, value: str) -> str:
         if "iphone" in value.lower():
             return "Phone"
         elif "mac" in value.lower():
