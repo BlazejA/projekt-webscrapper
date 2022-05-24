22155e91ed7ff184f7994e962ccdc5b76c33cbb8
DawidR2 <61984245+DawidR2@users.noreply.github.com>
2022-05-24 14:34:34 +0200

    MediaMarkt inf loop

--- Scrapper/Scrapper/spiders/MediaMarktScrapper.py
+++ Scrapper/Scrapper/spiders/MediaMarktScrapper.py
@@ -5,7 +5,8 @@ class MediaScrapper(scrapy.Spider):
     name = 'MediaMarkt'
 
     start_urls = [
-        'https://mediamarkt.pl/telefony-i-smartfony/smartfony/wszystkie-smartfony.apple'
+        'https://mediamarkt.pl/telefony-i-smartfony/smartfony/wszystkie-smartfony.apple',
+        #'https://mediamarkt.pl/komputery-i-tablety/laptopy-laptopy-2-w-1/notebooki.apple',
     ]
 
     custom_settings = {
@@ -15,68 +16,70 @@ class MediaScrapper(scrapy.Spider):
     def parse(self, response):
 
         for products in response.css('div.offer'):
+
+            if products.css('div.old-price span::text').get() is not None:
+
+                old_price = products.css('div.old-price span::text').get().strip()
+            else:
+                 old_price = None
             try:
-                if products.css('div.old-price span::text').get() is not None:
-
-                    old_price = products.css('div.old-price span::text').get().strip()
-                else:
-                     old_price = None
-
-                yield {
-                    'name': products.css('h2.title::text').get(),
-                    'actual_price': products.css('span.whole::text').get().strip(),
-                    'old_price': old_price,
-                    'category': self.GetCategoryByProductName(products.css('h2.title::text').get()),
-                    'shop': 'media_markt',
-                    'details': {
-                        'screen':
-                            {
-                                'screen_name': products.css('span.product-attribute-value::text').extract()[0:1][0].strip(),
-                                'internal_storage': products.css('span.product-attribute-value::text').extract()[2:3][0].strip(),
-                                'ram': products.css('span.product-attribute-value::text').extract()[3:4][0].strip(),
-                                'camera': products.css('span.product-attribute-value::text').extract()[4:5][0].strip(),
-                                'system': products.css('span.product-attribute-value::text').extract()[6:7][0].strip()
-                            }
-                    },
-                    'img': products.css('div.column.gallery div.spark-image').attrib['src'],
-                    'link': "https://mediamarkt.pl" + products.css('div.info a').attrib['href']
-                }
-
-                next_page = response.css('div.more-offers a').attrib['href']
-                if next_page is not None:
-                    yield response.follow("https://mediamarkt.pl"+next_page, callback=self.parse)
-
+                actual_price = products.css('span.whole::text').get().strip()
             except:
-                if products.css('div.old-price span::text').get() is not None:
+                actual_price= None
 
-                    old_price = products.css('div.old-price span::text').get().strip()
-                else:
-                    old_price = None
+            try:
+                system = products.css('span.product-attribute-value::text').extract()[6:7][0].strip()
+            except:
+                system = None
 
-                yield {
-                    'name': products.css('h2.title::text').get(),
-                    'actual_price': products.css('span.whole::text').get().strip(),
-                    'old_price': old_price,
-                    'category': self.GetCategoryByProductName(products.css('h2.title::text').get()),
-                    'shop': 'media_markt',
-                    'details': {
-                        'screen':
-                            {
-                                'screen_name': products.css('span.product-attribute-value::text').extract()[0:1][0].strip(),
-                                'internal_storage': products.css('span.product-attribute-value::text').extract()[2:3][
-                                    0].strip(),
-                                'ram': products.css('span.product-attribute-value::text').extract()[3:4][0].strip(),
-                                'camera': products.css('span.product-attribute-value::text').extract()[4:5][0].strip(),
-                                'system': None
-                            }
-                    },
-                    'img': products.css('div.column.gallery div.spark-image').attrib['src'],
-                    'link': "https://mediamarkt.pl" + products.css('div.info a').attrib['href']
-                }
+            try:
+                screen = products.css('span.product-attribute-value::text').extract()[0:1][0].strip()
+            except:
+                screen = None
 
-                next_page = response.css('div.more-offers a').attrib['href']
-                if next_page is not None:
-                    yield response.follow("https://mediamarkt.pl" + next_page, callback=self.parse)
+            try:
+                internal_storage = products.css('span.product-attribute-value::text').extract()[2:3][0].strip()
+            except:
+                internal_storage = None
+
+            try:
+                ram = products.css('span.product-attribute-value::text').extract()[3:4][0].strip()
+            except:
+                ram = None
+
+            try:
+                camera = products.css('span.product-attribute-value::text').extract()[4:5][0].strip()
+            except:
+                camera= None
+            yield {
+                'name': products.css('h2.title::text').get(),
+                'actual_price': actual_price,
+                'old_price': old_price,
+                'category': self.GetCategoryByProductName(products.css('h2.title::text').get()),
+                'shop': 'media_markt',
+                'details': {
+                    'screen':
+                        {
+                            'screen_name': screen,
+                            'internal_storage': internal_storage,
+                            'ram': ram,
+                            'camera': camera,
+                            'system': system
+                        }
+                },
+                'img': products.css('div.column.gallery div.spark-image').attrib['src'],
+                'link': "https://mediamarkt.pl" + products.css('div.info a').attrib['href']
+            }
+
+
+
+        next_page = response.css('div.more-offers a').attrib['href']
+        yield{'XXXXXXXXXX': next_page}
+        if next_page is not None:
+                yield response.follow("https://mediamarkt.pl" + next_page, callback=self.parse)
+
+
+
 
     def GetCategoryByProductName(self, value: str) -> str:
         if "iphone" in value.lower():
